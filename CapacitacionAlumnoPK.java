7fb7c253e8262cc6ca2beea12687f48053ebca76
aicalvopina <kyubi900@hotmail.com>
2017-06-26 00:22:54 -0500

    Update CapacitacionAlumnoPK.java

--- CapacitacionAlumnoPK.java
+++ CapacitacionAlumnoPK.java
@@ -4,34 +4,47 @@
  * and open the template in the editor.
  */
 package ec.edu.espe.EducaT;
-
+/**
+* Importacion de los paquetes necesarios para el funcionamiento del sistema.
+*/
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 /**
+ * Clase encargada de obtener los datos de la base de datos EducaTBD.
  *
- * @author Adrián
+ * @author Adrián Calvopiña, Jonathan Almeida, David Suarez.
+ * @version 1.0
  */
 @Embeddable
 public class CapacitacionAlumnoPK implements Serializable {
 
     @Basic(optional = false)
     @NotNull
-    @Column(name = "COD_CAPACITACION")
+    @Column(name = "COD_CAPACITACION") 
+    /**
+    * Atributo que representa a la clave de la tabla capacitacion.
+    */
     private int codCapacitacion;
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 10)
     @Column(name = "COD_ALUMNO")
+    /**
+    * Atributo que representa a la clave de la tabla alumno.
+    */
     private String codAlumno;
 
     public CapacitacionAlumnoPK() {
     }
-
+    /**
+    * Constructor de la clase.
+    * @param codCapacitacion representa a la clave de la tabla capacitacion. 
+    * @param codAlumno representa a la clave de la tabla alumno.
+    */
     public CapacitacionAlumnoPK(int codCapacitacion, String codAlumno) {
         this.codCapacitacion = codCapacitacion;
         this.codAlumno = codAlumno;
@@ -52,7 +65,10 @@ public class CapacitacionAlumnoPK implements Serializable {
     public void setCodAlumno(String codAlumno) {
         this.codAlumno = codAlumno;
     }
-
+    /**
+    * Metodo para obtner el codigo Hash
+    * @return hash devuelve el codigo hash. 
+    */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -60,7 +76,10 @@ public class CapacitacionAlumnoPK implements Serializable {
         hash += (codAlumno != null ? codAlumno.hashCode() : 0);
         return hash;
     }
-
+    /**
+    * Metodo para comparar objetos 
+    * @return devuelve el resultado de la comparacion 
+    */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -76,7 +95,10 @@ public class CapacitacionAlumnoPK implements Serializable {
         }
         return true;
     }
-
+    /**
+    * Metodo para enviar al objeto a una cadena de caracteres 
+    * @return devuelve la cadena de caracteres
+    */
     @Override
     public String toString() {
         return "ec.edu.espe.EducaT.CapacitacionAlumnoPK[ codCapacitacion=" + codCapacitacion + ", codAlumno=" + codAlumno + " ]";
