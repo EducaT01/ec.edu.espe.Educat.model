c90023c87dfe0e2def08cc18b159d9027ba51137
aicalvopina <kyubi900@hotmail.com>
2017-06-26 01:00:59 -0500

    Update ProgramaCursoPK.java

--- ProgramaCursoPK.java
+++ ProgramaCursoPK.java
@@ -4,17 +4,18 @@
  * and open the template in the editor.
  */
 package ec.edu.espe.EducaT;
-
+/**
+* Importacion de los paquetes necesarios para el funcionamiento del sistema.
+*/
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 /**
- *
- * @author Adrián
+ * Entidad que almacena los cursos que pertenecen a un programa.
+ * @author Adrián Calvopiña, Jonathan Almeida, David Suarez.
  */
 @Embeddable
 public class ProgramaCursoPK implements Serializable {
@@ -23,16 +24,28 @@ public class ProgramaCursoPK implements Serializable {
     @NotNull
     @Size(min = 1, max = 8)
     @Column(name = "COD_PROGRAMA")
+    /**
+    * Atributo que representa a la clave de la tabla programa.
+    */
     private String codPrograma;
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 8)
     @Column(name = "COD_CURSO")
+    /**
+    * Atributo que representa a la clave de la tabla curso.
+    */
     private String codCurso;
-
+    /*
+    *Constructor vacio de la clase.
+    */ 
     public ProgramaCursoPK() {
     }
-
+    /*
+    *Constructor de la clase.
+    * @param codPrograma Codigo del programa al que pertenece.
+    * @param codCurso  codigo del curso que lo conforma.
+    */ 
     public ProgramaCursoPK(String codPrograma, String codCurso) {
         this.codPrograma = codPrograma;
         this.codCurso = codCurso;
@@ -53,7 +66,10 @@ public class ProgramaCursoPK implements Serializable {
     public void setCodCurso(String codCurso) {
         this.codCurso = codCurso;
     }
-
+    /**
+    * Metodo para obtner el codigo Hash
+    * @return hash devuelve el codigo hash. 
+    */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -61,7 +77,10 @@ public class ProgramaCursoPK implements Serializable {
         hash += (codCurso != null ? codCurso.hashCode() : 0);
         return hash;
     }
-
+    /**
+    * Metodo para comparar objetos 
+    * @return devuelve el resultado de la comparacion 
+    */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -77,7 +96,10 @@ public class ProgramaCursoPK implements Serializable {
         }
         return true;
     }
-
+    /**
+    * Metodo para enviar al objeto a una cadena de caracteres 
+    * @return devuelve la cadena de caracteres
+    */
     @Override
     public String toString() {
         return "ec.edu.espe.EducaT.ProgramaCursoPK[ codPrograma=" + codPrograma + ", codCurso=" + codCurso + " ]";
